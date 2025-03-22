3472e57b71d8303c615881384c51321841ffcf1a
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2025-03-23 05:54:21 +0700

    fix: db

--- prisma/migrations/20250322225241_convertium/migration.sql
+++ prisma/migrations/20250322225241_convertium/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
-    "salutation" TEXT NOT NULL
+    "salutation" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
